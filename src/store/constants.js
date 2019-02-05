84bb5a89fe135cf29ca69b478176b6438801c215
malvin_j <george.arthur@nfortics.com>
2019-02-05 17:37:28 +0000

    added mandate status & mandate reference

--- src/store/constants.js
+++ src/store/constants.js
@@ -1,4 +1,4 @@
 // export const GET_BASE_URI = 'https://api-staging.flopay.io/api/'
 export const GET_BASE_URI = 'https://api.flopay.io/'
-// export const GET_BASE_URI = 'https://b8ec3401.ngrok.io/api/'
+// export const GET_BASE_URI = 'https://9ff5f9a3.ngrok.io/api/'
 export const CTRL_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
