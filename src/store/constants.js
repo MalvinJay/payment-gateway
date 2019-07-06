40afe3e2c08922261d8e3e5cbc10a15dd54713e9
malvin_j <george.arthur@nfortics.com>
2019-07-06 21:08:11 +0000

    purchases reports

--- src/store/constants.js
+++ src/store/constants.js
@@ -1,3 +1,3 @@
 export const GET_BASE_URI = 'https://api.flopay.io/'
-// export const GET_BASE_URI = 'https://d7ee5963.ngrok.io/'
+// export const GET_BASE_URI = 'https://cebff61c.ngrok.io/'
 export const CTRL_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
