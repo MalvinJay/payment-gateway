1c7e8e637a72f45c15a22500d225029a62149a1e
malvin_j <george.arthur@nfortics.com>
2019-09-13 16:39:12 +0000

    Second Build

--- src/store/constants.js
+++ src/store/constants.js
@@ -1,3 +1,3 @@
-// export const GET_BASE_URI = 'https://api.flopay.io/'
-export const GET_BASE_URI = 'https://f7213f21.ngrok.io/'
+export const GET_BASE_URI = 'https://api.flopay.io/'
+// export const GET_BASE_URI = 'https://f7213f21.ngrok.io/'
 export const CTRL_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
