4b013f4f4920756b46abdb7162396e655bd16b69
malvin_j <george.arthur@nfortics.com>
2019-10-01 11:03:54 +0000

    Stocks Report

--- src/store/constants.js
+++ src/store/constants.js
@@ -1,3 +1,4 @@
-export const GET_BASE_URI = 'https://api.flopay.io/'
-// export const GET_BASE_URI = 'https://f7213f21.ngrok.io/'
 export const CTRL_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+export const STAGING_URL = 'http://a5a392cf.ngrok.io/'
+export const PRODUCTION_URL = 'https://api.flopay.io/'
+export const GET_BASE_URI = process.env.NODE_ENV === 'development'? STAGING_URL : PRODUCTION_URL
